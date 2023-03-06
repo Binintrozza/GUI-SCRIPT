a5a2c24974121a96850740ea948fa767f4daae49
Binintrozza <114798173+Binintrozza@users.noreply.github.com>
2023-03-06 22:55:24 +0700

    Create YUTO HUB .lua

---
+++ YUTO HUB .lua
@@ -0,0 +1,17 @@
+local Yutohub = loadstring(game:HttpGet(('https://raw.githubusercontent.com/Binintrozza/GUI-SCRIPT/main/GUI_NEW.lua')))()
+
+if game.PlaceId == 8540346411 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Binintrozza/GUI-SCRIPT/main/REBIRTH%20CHAMPION%20%5BYuto%20Hub%5D.lua"))()
+elseif game.PlaceId == 11561748530 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Binintrozza/ev2evwvw/main/GUI.lua"))()
+else 
+    Yutohub:MakeNotification({
+        Name = "DONT SUPPORT THIS GAME",
+        Content = "Check Features ON DISCORD SEVER ( YUTO HUB | COMMUNITY )",
+        Image = "rbxassetid://12699767780",
+        Time = 10
+    })
+    
+
+    Yutohub:Init()
+end
